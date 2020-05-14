0ec6931e69a609fe3caf61968a976ce935f62fa5
Andrius Mozūraitis <andrius.mozuraitis@searchnode.net>
2020-05-14 17:06:55 +0300

    feature: Creating file if it do not exsists

--- src/base.ts
+++ src/base.ts
@@ -1,5 +1,5 @@
 import { createReadStream, createWriteStream } from "fs"
-
+import { ensureFileSync } from 'fs-extra'
 export type Source = string | NodeJS.ReadableStream
 export type Output = string | NodeJS.WriteStream
 
@@ -13,8 +13,8 @@ export function sourceToReadStream(data: Source): NodeJS.ReadableStream {
 
 
 export function outputToWriteStream(data: Output): NodeJS.WritableStream {
-    console.log(data)
     if (typeof data === 'string') {
+        ensureFileSync(data)
         return createWriteStream(data)
     }
     return data
